eda2d8ca3087941b284d78c8eaadc0fd8e13ce45
Администратор <gala6ap@mail.ru>
2016-11-10 00:43:53 +0200

    Module 3 task 2 was added

---
+++ src/module3/task2/Adder.java
@@ -0,0 +1,11 @@
+package module3.task2;
+
+/**
+ * Created by Администратор on 08.11.16.
+ */
+public class Adder extends Arithmetic {
+    public boolean check(Integer a, Integer b)
+    {
+        return a>=b;
+    }
+}
---
+++ src/module3/task2/Arithmetic.java
@@ -0,0 +1,12 @@
+package module3.task2;
+
+/**
+ * Created by Администратор on 08.11.16.
+ */
+public class Arithmetic {
+    public int add(Integer a, Integer b)
+    {
+        // Unboxing Integer to int
+        return  a+b;
+    }
+}
